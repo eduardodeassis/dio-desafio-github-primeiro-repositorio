30c3e0b895b8521584476b055426e9b3c45e3c8b
eduardodeassis <eduardodeassis@gmail.com>
2021-12-28 16:32:39 -0300

    JavaScript - Colocando em prática - Criação de um contador

--- Introdução ao JavaScript/dio/assets/js/scripts.js
+++ Introdução ao JavaScript/dio/assets/js/scripts.js
@@ -1,1 +1,35 @@
-console.log('hello world');
+console.log('Contador');
+
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+var buttonAdicionarWrapper = document.getElementsByName('adicionar')[0];
+var buttonSubtrairWrapper = document.getElementsByName('subtrair')[0];
+
+buttonAdicionarWrapper.addEventListener('click', testarBotao);
+buttonSubtrairWrapper.addEventListener('click', testarBotao);
+
+function testarBotao() {
+    if (currentNumber >= 0) {
+        currentNumberWrapper.style.color = '';
+    } else {
+        currentNumberWrapper.style.color = 'red';
+    }
+    buttonAdicionarWrapper.removeAttribute('disabled');
+    buttonSubtrairWrapper.removeAttribute('disabled');
+    if (currentNumber >= 10) {
+        buttonAdicionarWrapper.setAttribute('disabled', 'disabled');
+    }
+    if (currentNumber <= -10) {
+        buttonSubtrairWrapper.setAttribute('disabled', 'disabled');
+    }
+}
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;    
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;    
+}
