5c9eda6d1e401d6239805ae2e0b1070a8f94c79b
eduardodeassis <eduardodeassis@gmail.com>
2021-12-28 01:52:44 -0300

    Introdução ao JavaScript e exemplos de comentários e declarações de funções

---
+++ Introdução ao JavaScript/teste.js
@@ -0,0 +1,10 @@
+//var y=0; //comentário de linha -> ctrl + /
+
+/*var y=0;
+var x=1;*/ //comentário de bloco
+
+function soma(a, bloco) {
+    //exemplo de declaração de função
+    console.log(a + b);
+    return a + b;
+}
