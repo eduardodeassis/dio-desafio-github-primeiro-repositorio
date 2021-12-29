b30834171a6d90a2f1fbcc58b8bf4870d88cfd51
eduardodeassis <eduardodeassis@gmail.com>
2021-12-29 00:29:56 -0300

    JavaScript - Desafio to-do list - Alterado para corrigir o erro de perder as tarefas marcadas.

--- Introdução ao JavaScript/to-do-list/assets/js/scripts.js
+++ Introdução ao JavaScript/to-do-list/assets/js/scripts.js
@@ -1,12 +1,37 @@
 console.log('To-do list');
 
+const form = document.getElementById('task-form');
 var textoItem = document.getElementById('to-do-item');
 const listItensWrapper = document.getElementById('list-items');
 var item = 0; 
 
+form.onsubmit = function (e) {
+	e.preventDefault();
+	adicionar();
+	form.reset();
+};
+
 function adicionar() {
-    var texto = textoItem.value;
-    listItensWrapper.innerHTML+='<input type="checkbox" id="item'+item+'" value="teste'+item+'"><label for="item'+item+'">'+texto+'</label><br>';
-    var itemWrapper = document.getElementById('item'+item);
+    var texto = textoItem.value;   
+    var idName = 'item'+item; 
+
+    const taskContainer = document.createElement('div');
+	const newTask = document.createElement('input');
+	const taskLabel = document.createElement('label');
+	const taskDescriptionNode = document.createTextNode(texto);
+
+	newTask.setAttribute('type', 'checkbox');
+	newTask.setAttribute('name', idName);
+	newTask.setAttribute('id', idName);
+
+	taskLabel.setAttribute('for', idName);
+	taskLabel.appendChild(taskDescriptionNode);
+
+	taskContainer.classList.add('list-items');
+	taskContainer.appendChild(newTask);
+	taskContainer.appendChild(taskLabel);
+
+	listItensWrapper.appendChild(taskContainer);
+
     item+=1;
 }
