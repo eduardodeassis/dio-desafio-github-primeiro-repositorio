3e04d2eaf6f5c2e205a3b211ceda3eec34de92b3
eduardodeassis <eduardodeassis@gmail.com>
2021-12-31 01:35:59 -0300

    Aula 6 - Aprofundando em funções

---
+++ Sintaxe básica em JavaScript/Aula 6 - Aprofundando em funções/scripts.js
@@ -0,0 +1,29 @@
+//funções declarativas
+function funcaoDeclarativa() {
+    console.log('Função declarativa');
+}
+
+funcaoDeclarativa();    
+
+//Expressões de funções (pode ser nomeada ou não)
+var funcaoDeExpressao = function() {
+  console.log('Expressões de funções');
+}
+funcaoDeExpressao();
+
+//Arrow function
+var arrowFunction = () => {
+    console.log('Arrow function');
+}
+
+arrowFunction();
+
+//Number() – para converter valores em números;
+//Prompt() – para registrar entradas de usuário;
+//Alert() – para mostrar mensagem ao usuário;
+//Template Strings – para usar strings junto com expressões;
+
+//Funções – https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/
+//Functions Number – https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Number Window.prompt – https://developer.mozilla.org/pt-BR/docs/Web/API/Window/prompt
+//Window.alert – https://developer.mozilla.org/pt-BR/docs/Web/API/Window/alert
+//Template String - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Template_literals
