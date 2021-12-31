95ffac07d5d82f001a26456fa7bb2a3c0f5c7e6f
eduardodeassis <eduardodeassis@gmail.com>
2021-12-31 01:03:13 -0300

    Aula 5 - Funções e suas particularidades

---
+++ Sintaxe básica em JavaScript/Aula 5 - Funções e as suas particularidades/scripts.js
@@ -0,0 +1,13 @@
+//função
+function funcao() {
+    console.log('Teste de execução de uma função');
+}
+
+funcao();
+
+// função com parametros
+function mensagem(paramA, paramB) {
+    console.log(paramA, paramB);
+}
+
+mensagem('Tudo certo', 'jovem!');
